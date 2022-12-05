b70a1c47c3105afc4f93d6838e20d88e6f9ee44e
mateusfelixdias <felixmateus618@gmail.com>
2022-12-05 09:40:51 -0300

    feat: added product model

---
+++ server/src/database/models/Product.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from 'mongoose';
+import { IProduct } from '../../interfaces/Product';
+
+const productSchema = new Schema<IProduct>(
+  {
+    category: { type: Schema.Types.ObjectId, required: true, ref: 'Category' },
+    decription: { type: String, required: true },
+    imagePath: { type: String, required: true },
+    ingredients: {
+      type: [
+        {
+          icon: { type: String, required: true },
+          name: { type: String, required: true },
+        },
+      ],
+      required: true,
+    },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Product = model('Product', productSchema);
