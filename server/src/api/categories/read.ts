5ed80983d08194ed143b6044fec895efa7449d7b
mateusfelixdias <felixmateus618@gmail.com>
2022-12-05 09:29:14 -0300

    feat: added handle read

--- server/src/api/categories/read.ts
+++ server/src/api/categories/read.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { Category } from '../../database/models/Category';
-import { handleRead } from '../../utils/validation/categories/read';
 
 export async function read(request: Request, response: Response) {
   const categories = await Category.find();
@@ -8,4 +7,12 @@ export async function read(request: Request, response: Response) {
   const { status, result } = handleRead(categories);
 
   return response.status(status).json(result).end();
+}
+
+function handleRead(categories: any | []) {
+  if (!categories.length) {
+    return { result: 'Não têm nenhuma categoria cadastrada!', status: 404 };
+  }
+
+  return { result: categories, status: 200 };
 }
--- server/src/api/products/read.ts
+++ server/src/api/products/read.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { Product } from '../../database/models/Product';
-import { handleRead } from '../../utils/validation/products/read';
 
 export async function read(request: Request, response: Response) {
   const products = await Product.find();
@@ -8,4 +7,12 @@ export async function read(request: Request, response: Response) {
   const { result, status } = handleRead(products);
 
   return response.status(status).json(result).end();
+}
+
+function handleRead(categories: any | []) {
+  if (!categories.length) {
+    return { result: 'Não têm nenhum produto cadastrado!', status: 404 };
+  }
+
+  return { result: categories, status: 200 };
 }
