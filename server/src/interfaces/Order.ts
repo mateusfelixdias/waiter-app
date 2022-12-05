2dcfdd4aadb9e9dbd7acaefd6b1fb2a7cd52f0f2
mateusfelixdias <felixmateus618@gmail.com>
2022-12-05 09:32:40 -0300

    feat: added interface order

---
+++ server/src/interfaces/Order.ts
@@ -0,0 +1,8 @@
+import { Schema } from 'mongoose';
+
+export interface IOrder {
+  table: string;
+  createdAt: Date;
+  status: 'WAITING' | 'IN_PRODUCTION' | 'DONE';
+  products: Array<{ product: Schema.Types.ObjectId; quantity: number }>;
+}
