dc02bc431f1c54aaa32d50fcfbc18e191b2f1160
mateusfelixdias <felixmateus618@gmail.com>
2022-12-11 14:26:25 -0300

    feat: added image upload

--- server/src/routes/products/routes.ts
+++ server/src/routes/products/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { read } from '../../api/products/read';
 import { create } from '../../api/products/create';
+import { upload } from '../../api/products/upload';
 
 export const products = Router();
 
-products.post('/products', create);
+products.post('/products', upload.single('image'), create);
 
 products.get('/products', read);
