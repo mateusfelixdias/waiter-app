473a1b5c1b430f6dd7991db1e9c9510aa81e67d1
mateusfelixdias <felixmateus618@gmail.com>
2022-12-11 14:23:24 -0300

    feat: added route in read product by category

--- server/src/routes/categories/routes.ts
+++ server/src/routes/categories/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { create } from '../../api/categories/create';
 import { read } from '../../api/categories/read';
+import { create } from '../../api/categories/create';
+import { readProductsByCategory } from '../../api/categories/readProductsByCategory';
 
 export const categories = Router();
 
@@ -8,4 +9,4 @@ categories.post('/categories', create);
 
 categories.get('/categories', read);
 
-categories.get('/categories/:categoryId/products');
+categories.get('/categories/:categoryId/products', readProductsByCategory);
