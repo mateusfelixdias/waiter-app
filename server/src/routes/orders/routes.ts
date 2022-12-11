1e9dd44772de843b82dc600ff7117beb2a2546a2
mateusfelixdias <felixmateus618@gmail.com>
2022-12-11 14:24:49 -0300

    feat: added route in create, read, update and delete

--- server/src/routes/orders/routes.ts
+++ server/src/routes/orders/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import { read } from '../../api/orders/read';
+import { create } from '../../api/orders/create';
+import { update } from '../../api/orders/update';
+import { destroy } from '../../api/orders/destroy';
 
 export const orders = Router();
 
-orders.post('/orders');
+orders.post('/orders', create);
 
-orders.get('/orders');
+orders.get('/orders', read);
 
-orders.patch('/orders/:orderId');
+orders.patch('/orders/:orderId', update);
 
-orders.delete('/orders/:orderId');
+orders.delete('/orders/:orderId', destroy);
