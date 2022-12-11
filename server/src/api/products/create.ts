69b47d0ced051885cc14286fe8c34ec2d9065855
mateusfelixdias <felixmateus618@gmail.com>
2022-12-11 14:15:31 -0300

    feat: added image path

--- server/src/api/products/create.ts
+++ server/src/api/products/create.ts
@@ -2,11 +2,26 @@ import { Request, Response } from 'express';
 import { Product } from '../../database/models/Product';
 import { handleValidationProduct } from '../../utils/validation/products/create';
 
-export async function create({ body }: Request, response: Response) {
-  const validationMessage = handleValidationProduct(body);
+export async function create(request: Request, response: Response) {
+  const { body, file } = request;
+
+  const imagePath = file?.filename || '';
+  const ingredientsConvertedToObj = body?.ingredients
+    ? JSON.parse(body.ingredients)
+    : [];
+
+  const validationMessage = handleValidationProduct({
+    ...body,
+    imagePath,
+    ingredients: ingredientsConvertedToObj,
+  });
 
   if (validationMessage === true) {
-    const product = await Product.create({ ...body });
+    const product = await Product.create({
+      ...body,
+      imagePath,
+      ingredients: ingredientsConvertedToObj,
+    });
 
     return response.status(201).json(product).end();
   }
