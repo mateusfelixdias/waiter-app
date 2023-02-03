fe0f33218003151a85a474593ac967275e4f9d18
mateusfelixdias <felixmateus618@gmail.com>
2023-02-03 06:20:55 -0300

    feat: added App

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from './components/Header';
+import { Orders } from './components/Orders';
+
+export function App() {
+  return (
+    <>
+      <Header />
+
+      <Orders />
+    </>
+  );
+}
