703eab29386ed4520f75f466135a5acf0a79e1ab
mateusfelixdias <felixmateus618@gmail.com>
2023-05-25 13:18:22 -0300

    feat: connecting client with server

--- frontend/src/components/Orders/index.tsx
+++ frontend/src/components/Orders/index.tsx
@@ -1,13 +1,81 @@
+import { api } from '../../services';
+import socketIo from 'socket.io-client';
+import { useEffect, useState } from 'react';
 import { OrdersBoard } from '../OrdersBoard';
+import { IOrder } from '../../interfaces/Order';
 
 import './index.css';
 
 export function Orders() {
+  const [orders, setOrders] = useState<IOrder[]>([]);
+
+  useEffect(() => {
+    const socket = socketIo('http://localhost:3000', {
+      transports: ['websocket'],
+    });
+
+    socket.on('orders@new', (order) => {
+      setOrders((prevState) => prevState.concat(order));
+    });
+  }, []);
+
+  useEffect(() => {
+    const handleOrders = async () => {
+      try {
+        const { data, status } = await api.get('/orders');
+
+        if (status === 200) setOrders(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleOrders();
+  }, []);
+
+  const done = orders.filter(({ status }) => status === 'DONE');
+  const waiting = orders.filter(({ status }) => status === 'WAITING');
+  const inProduction = orders.filter(({ status }) => {
+    return status === 'IN_PRODUCTION';
+  });
+
+  const handleCancelOrder = (orderId: string) => {
+    setOrders((prevState) => prevState.filter(({ _id }) => _id !== orderId));
+  };
+
+  const handleOrderStatusChange = (
+    orderId: string,
+    status: IOrder['status']
+  ) => {
+    setOrders((prevState) => {
+      return prevState.map((order) => {
+        return orderId === order._id ? { ...order, status } : order;
+      });
+    });
+  };
+
   return (
     <div className="conteiner">
-      <OrdersBoard />
-      <OrdersBoard />
-      <OrdersBoard />
+      <OrdersBoard
+        orders={waiting}
+        title="Fila de espera"
+        onCancelOrder={handleCancelOrder}
+        onOrderStatusChange={handleOrderStatusChange}
+      />
+
+      <OrdersBoard
+        title="Em preparação"
+        orders={inProduction}
+        onCancelOrder={handleCancelOrder}
+        onOrderStatusChange={handleOrderStatusChange}
+      />
+      
+      <OrdersBoard
+        orders={done}
+        title="Pronto"
+        onCancelOrder={handleCancelOrder}
+        onOrderStatusChange={handleOrderStatusChange}
+      />
     </div>
   );
 }
--- frontend/src/components/OrdersBoard/index.tsx
+++ frontend/src/components/OrdersBoard/index.tsx
@@ -1,26 +1,105 @@
+import { useState } from 'react';
+import { api } from '../../services';
+import { toast } from 'react-toastify';
+import { OrderModal } from '../OrderModal';
+import { IOrder } from '../../interfaces/Order';
+import { IOrdersBoard } from '../../interfaces/OrdersBoard';
+
 import './index.css';
 
-export const OrdersBoard = () => {
+export const OrdersBoard = ({
+  title,
+  orders,
+  onCancelOrder,
+  onOrderStatusChange,
+}: IOrdersBoard) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [selectedOrder, setSelectedOrder] = useState<IOrder | null>(null);
+
+  const handleOpenModal = (order: IOrder) => {
+    setIsModalVisible(true);
+    setSelectedOrder(order);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedOrder(null);
+    setIsModalVisible(false);
+  };
+
+  const handleOrderStatusChange = async () => {
+    try {
+      const { status } = selectedOrder || {};
+      const newStatus = status === 'WAITING' ? 'IN_PRODUCTION' : 'DONE';
+
+      await api.patch(`/orders/${selectedOrder!._id}`, { status: newStatus });
+
+      onOrderStatusChange(selectedOrder!._id, newStatus);
+
+      const { table } = selectedOrder || {};
+      toast.success(`O status dá mesa ${table} foi alterado!`);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+      setIsModalVisible(false);
+    }
+  };
+
+  const handleCancelOrder = async () => {
+    try {
+      setIsLoading(true);
+
+      await api.delete(`/orders/${selectedOrder!._id}`);
+
+      const { table } = selectedOrder || {};
+      toast.success(`O pedido dá mesa ${table} foi cancelado!`);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      onCancelOrder(selectedOrder!._id);
+
+      setIsLoading(false);
+      setIsModalVisible(false);
+    }
+  };
+
   return (
-    <div className="board-conteiner">
-      <header>
-        <strong>Fila de espera</strong>
-        <span>(1)</span>
-      </header>
+    <>
+      <OrderModal
+        order={selectedOrder}
+        isLoading={isLoading}
+        visible={isModalVisible}
+        onClose={handleCloseModal}
+        onCancelOrder={handleCancelOrder}
+        onOrderStatusChange={handleOrderStatusChange}
+      />
 
-      <div className="orders-conteiner">
-        <button type="button">
-          <strong>Mesa 2</strong>
+      <div className="board-conteiner">
+        <header>
+          <strong>{title}</strong>
+          <span>(1)</span>
+        </header>
 
-          <span>2 Itens</span>
-        </button>
+        <div className="orders-conteiner">
+          {orders.map((order) => {
+            const { _id, products, table } = order;
+            const length = products.length;
 
-        <button type="button">
-          <strong>Mesa 2</strong>
+            return (
+              <button
+                key={_id}
+                type="button"
+                onClick={() => handleOpenModal(order)}
+              >
+                <strong>Mesa {table}</strong>
 
-          <span>2 Itens</span>
-        </button>
+                <span>{length} Item(s)</span>
+              </button>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
